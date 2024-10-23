6ff5a6db4e795d689d849464934f32b64cae47b7
Diego Vargas <dialvaro30@gmail.com>
2024-10-23 17:50:32 -0500

    Add mike42/escpos-php library for printing and open-drawer API endpoint

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,10 @@
 <?php
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Route;
-
+use Mike42\Escpos\PrintConnectors\WindowsPrintConnector;
+use Mike42\Escpos\Printer;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -16,4 +18,20 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+Route::post('open-drawer', function () {
+    Log::info('openDrawer');
+    $connector = new WindowsPrintConnector('POS-80');
+
+    $printer = new Printer($connector);
+
+    try {
+        // Comando ESC/POS para abrir la caja registradora
+        $printer->pulse();
+        $printer->close();
+
+        return response()->json(['message' => 'Caja abierta'], 200);
+    } catch (\Exception $e) {
+        return response()->json(['message' => 'Error al abrir la caja', 'error' => $e->getMessage()], 500);
+    }
 });
